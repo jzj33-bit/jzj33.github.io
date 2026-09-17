2361afaf0b516b0197d6e69ef6bc156084becf50
jzj33 <jzj33@txstate.edu>
2026-09-16 19:47:47 -0500

    Reverted all work back to step 2 on lab4

--- javascript/external.js
+++ javascript/external.js
@@ -1,8 +1,18 @@
-
-for (var i=0; i < arr.length; i++)
+//
+// for (var i=0; i < arr.length; i++)
+// {
+//   document.writeln(arr[i],"<br>");
+// }
+// // A prompt box is used to prompt users to input a value before entering a page.
+// user_name = window.prompt("Please enter your name", "Type your name here");
+// dsocument.write(user_name);
+function welcome()
 {
-  document.writeln(arr[i],"<br>");
+  let a = "Please enter your name.";
+  let b = "Type your name here.";
+// A prompt box is used to prompt users to input a vlaue before entering a page.
+  user_name = window.prompt(a, b);
+  message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+  return message
 }
-// A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt("Please enter your name", "Type your name here");
-dsocument.write(user_name);
+document.write(welcome());
