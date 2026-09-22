3428a28e741da29031cae4761449370329b5a634
jzj33 <jzj33@txstate.edu>
2026-09-22 09:30:53 -0500

    Filled content from webmaps array into the webmaps table

--- javascript/external.js
+++ javascript/external.js
@@ -6,6 +6,11 @@
 // // A prompt box is used to prompt users to input a value before entering a page.
 // user_name = window.prompt("Please enter your name", "Type your name here");
 // dsocument.write(user_name);
+var webmaps =
+[
+  ["Oil Spill Toolkit", "https://www.glo.texas.gov", "The oil spill toolkit developed by Enterprise Technology Solutions is neat."],
+  ["Texas Ecosystems Analytical Mapper", "http://tpwd.texas.gov/gis/team/", "The Texas Parks and Wildlife's Landscape Ecology program is great."]
+]
 function welcome()
 {
   let a = "Please enter your name.";
@@ -18,16 +23,15 @@ function welcome()
 function webmap_table()
 {
 document.write("<table width=100%>");
-for (var row=0; row < 2; row++)
+for (var row=0; row < webmaps.length; row++)
 {
   document.write("<tr>");
-  for (var column=0; column <3; column++)
+  for (var column=0; column < webmaps[0].length; column++)
   {
-    document.write("<td>" + row + "," + column + "</td>");
+    document.write("<td>" + webmaps[row][column] + "</td>");
   }
   document.write("</tr>");
 }
 document.write("</table>");
 return "";
 }
-  
